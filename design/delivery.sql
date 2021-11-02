38590812b2c31343f63b620315b8348f86bf5c2e
hftamayo <hftamayo@gmail.com>
2021-11-02 12:17:37 -0600

    updating readme.md file

--- design/delivery.sql
+++ design/delivery.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `invoice`
 --
 
+CREATE SCHEMA IF NOT EXISTS `delivery` DEFAULT CHARACTER SET utf8mb3 ;
+USE `delivery`;
+
 DROP TABLE IF EXISTS `invoice`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
